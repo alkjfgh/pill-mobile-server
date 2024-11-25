f3905973e946a260dbe5cbc623baad0524cf532f
alkjf <alkjfgh@naver.com>
2024-11-25 14:33:18 +0900

    fix

--- app/api/router.py
+++ app/api/router.py
@@ -7,4 +7,4 @@ router = APIRouter()
 router.include_router(users.router, prefix="/users", tags=["users"])
 
 # 알약 이미지 판별 관련 API
-router.include_router(disPill.router, prefix="/disPill", tags=["disPill"])
+router.include_router(disPill.router, prefix="/disPill", tags=["discrimination pill"])
