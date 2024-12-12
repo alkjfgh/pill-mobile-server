839edc95bde65f629b374e18072fe07075edad78
alkjf <alkjfgh@naver.com>
2024-12-12 14:55:50 +0900

    fix import location

--- app/services/DisImageService.py
+++ app/services/DisImageService.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import os
-from core.flowerTrans import FlowerTrans
+from app.core.flowerTrans import FlowerTrans
 
 
 class DisImageService:
